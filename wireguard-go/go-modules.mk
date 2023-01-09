86154894847491d8237e247e2cd6830eb8aeed09
Jonathan Perkin <jonathan@perkin.org.uk>
2023-01-09 10:57:12 +0000

    wireguard-go: Import version 0.0.20220316.
    Based on https://github.com/omniosorg/omnios-extra/pull/1235, will need
    to live in pkgsrc-extra until nshalman fork is merged upstream.

---
+++ wireguard-go/go-modules.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+GO_MODULE_FILES+=	github.com/google/btree/@v/v1.0.1.mod
+GO_MODULE_FILES+=	github.com/google/btree/@v/v1.0.1.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20220315160706-3147a52a75dd.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20220315160706-3147a52a75dd.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20220225172249-27dd8689420f.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20220225172249-27dd8689420f.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20221010170243-090e33056c14.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20221010170243-090e33056c14.zip
+GO_MODULE_FILES+=	golang.org/x/time/@v/v0.0.0-20191024005414-555d28b269f0.mod
+GO_MODULE_FILES+=	golang.org/x/time/@v/v0.0.0-20191024005414-555d28b269f0.zip
+GO_MODULE_FILES+=	golang.zx2c4.com/wintun/@v/v0.0.0-20211104114900-415007cec224.mod
+GO_MODULE_FILES+=	golang.zx2c4.com/wintun/@v/v0.0.0-20211104114900-415007cec224.zip
+GO_MODULE_FILES+=	gvisor.dev/gvisor/@v/v0.0.0-20220817001344-846276b3dbc5.mod
+GO_MODULE_FILES+=	gvisor.dev/gvisor/@v/v0.0.0-20220817001344-846276b3dbc5.zip
