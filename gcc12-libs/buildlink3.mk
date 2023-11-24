83ec13fb4ade1c115334fa7e73f0cdd3284c5aae
Jonathan Perkin <jonathan@perkin.org.uk>
2023-11-24 11:43:40 +0000

    gcc*-libs: Enhance the override specs.
    With our GCC packages encoding LOCALBASE as a default search path to
    help out-of-pkgsrc builds for regular users, this turns out to subvert
    buildlink3 as libraries were being pulled in directly if they happened
    to be installed.  The new implicit DEPENDS checks helped to expose these
    problems.
    
    To avoid this happening we now override the link flag specs to remove
    LOCALBASE from the default search path.
    
    While here remove some redundant USE_PKGSRC_GCC_RUNTIME checks, these
    files are only ever included if that is already set.

--- gcc12-libs/buildlink3.mk
+++ gcc12-libs/buildlink3.mk
@@ -5,35 +5,31 @@ BUILDLINK_TREE+=	gcc12-libs
 .if !defined(GCC12_LIBS_BUILDLINK3_MK)
 GCC12_LIBS_BUILDLINK3_MK:=
 
-.if ${USE_PKGSRC_GCC_RUNTIME:U:tl} == yes
 BUILDLINK_API_DEPENDS.gcc12-libs+=	gcc12-libs>=12.2.0
-.else
-BUILDLINK_API_DEPENDS.gcc12-libs+=	gcc12>=12.2.0
-.endif
 BUILDLINK_PKGSRCDIR.gcc12-libs=		../../extra/gcc12-libs
 BUILDLINK_DEPMETHOD.gcc12-libs?=	full
 
 BUILDLINK_PASSTHRU_DIRS+=	${BUILDLINK_PREFIX.gcc12-libs}/gcc12
-
 BUILDLINK_FILES.gcc12-libs=	# empty
 BUILDLINK_AUTO_VARS.gcc12-libs=	no
 
-.if ${USE_PKGSRC_GCC_RUNTIME:U:tl} == yes
-# Use custom specs file to ensure we link against pkgsrc libraries.
-SPECS_LIBGCC=		${WRAPPER_DIR}/specs.libgcc
-WRAPPER_TARGETS+=	${SPECS_LIBGCC}
+# Use custom specs file to ensure we link against the GCC libraries from
+# this package, as well as sanitising the default library search path.
+SPECS_PKGSRC=		${WRAPPER_DIR}/specs.pkgsrc
+WRAPPER_TARGETS+=	${SPECS_PKGSRC}
 
-${SPECS_LIBGCC}:
-	@${ECHO} "*link_libgcc:" >${SPECS_LIBGCC}
-	@${ECHO} "%D ${LINKER_RPATH_FLAG}${BUILDLINK_PREFIX.gcc12-libs}/gcc12/${MACHINE_GNU_PLATFORM}/lib/%M" >>${SPECS_LIBGCC}
+${SPECS_PKGSRC}:
+	${SED} -e \
+	    's,@LIBGCC_PREFIX@,${BUILDLINK_PREFIX.gcc12-libs}/gcc12/${MACHINE_GNU_PLATFORM},g' \
+	    < ${BUILDLINK_PKGSRCDIR.gcc12-libs}/files/specs.pkgsrc \
+	    > ${SPECS_PKGSRC}
 
-_WRAP_EXTRA_ARGS.CC+=	-specs=${SPECS_LIBGCC}
-_WRAP_EXTRA_ARGS.CXX+=	-specs=${SPECS_LIBGCC}
-_WRAP_EXTRA_ARGS.FC+=	-specs=${SPECS_LIBGCC}
-CWRAPPERS_APPEND.cc+=	-specs=${SPECS_LIBGCC}
-CWRAPPERS_APPEND.cxx+=	-specs=${SPECS_LIBGCC}
-CWRAPPERS_APPEND.f77+=	-specs=${SPECS_LIBGCC}
-.endif
+_WRAP_EXTRA_ARGS.CC+=	-specs=${SPECS_PKGSRC}
+_WRAP_EXTRA_ARGS.CXX+=	-specs=${SPECS_PKGSRC}
+_WRAP_EXTRA_ARGS.FC+=	-specs=${SPECS_PKGSRC}
+CWRAPPERS_APPEND.cc+=	-specs=${SPECS_PKGSRC}
+CWRAPPERS_APPEND.cxx+=	-specs=${SPECS_PKGSRC}
+CWRAPPERS_APPEND.f77+=	-specs=${SPECS_PKGSRC}
 
 .include "../../mk/dlopen.buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
--- gcc13-libs/buildlink3.mk
+++ gcc13-libs/buildlink3.mk
@@ -5,35 +5,31 @@ BUILDLINK_TREE+=	gcc13-libs
 .if !defined(GCC13_LIBS_BUILDLINK3_MK)
 GCC13_LIBS_BUILDLINK3_MK:=
 
-.if ${USE_PKGSRC_GCC_RUNTIME:U:tl} == yes
-BUILDLINK_API_DEPENDS.gcc13-libs+=	gcc13-libs>=13.1.0
-.else
-BUILDLINK_API_DEPENDS.gcc13-libs+=	gcc13>=13.1.0
-.endif
+BUILDLINK_API_DEPENDS.gcc13-libs+=	gcc13-libs>=13.2.0
 BUILDLINK_PKGSRCDIR.gcc13-libs=		../../extra/gcc13-libs
 BUILDLINK_DEPMETHOD.gcc13-libs?=	full
 
 BUILDLINK_PASSTHRU_DIRS+=	${BUILDLINK_PREFIX.gcc13-libs}/gcc13
-
 BUILDLINK_FILES.gcc13-libs=	# empty
 BUILDLINK_AUTO_VARS.gcc13-libs=	no
 
-.if ${USE_PKGSRC_GCC_RUNTIME:U:tl} == yes
-# Use custom specs file to ensure we link against pkgsrc libraries.
-SPECS_LIBGCC=		${WRAPPER_DIR}/specs.libgcc
-WRAPPER_TARGETS+=	${SPECS_LIBGCC}
+# Use custom specs file to ensure we link against the GCC libraries from
+# this package, as well as sanitising the default library search path.
+SPECS_PKGSRC=		${WRAPPER_DIR}/specs.pkgsrc
+WRAPPER_TARGETS+=	${SPECS_PKGSRC}
 
-${SPECS_LIBGCC}:
-	@${ECHO} "*link_libgcc:" >${SPECS_LIBGCC}
-	@${ECHO} "%D ${LINKER_RPATH_FLAG}${BUILDLINK_PREFIX.gcc13-libs}/gcc13/${MACHINE_GNU_PLATFORM}/lib/%M" >>${SPECS_LIBGCC}
+${SPECS_PKGSRC}:
+	${SED} -e \
+	    's,@LIBGCC_PREFIX@,${BUILDLINK_PREFIX.gcc13-libs}/gcc13/${MACHINE_GNU_PLATFORM},g' \
+	    < ${BUILDLINK_PKGSRCDIR.gcc13-libs}/files/specs.pkgsrc \
+	    > ${SPECS_PKGSRC}
 
-_WRAP_EXTRA_ARGS.CC+=	-specs=${SPECS_LIBGCC}
-_WRAP_EXTRA_ARGS.CXX+=	-specs=${SPECS_LIBGCC}
-_WRAP_EXTRA_ARGS.FC+=	-specs=${SPECS_LIBGCC}
-CWRAPPERS_APPEND.cc+=	-specs=${SPECS_LIBGCC}
-CWRAPPERS_APPEND.cxx+=	-specs=${SPECS_LIBGCC}
-CWRAPPERS_APPEND.f77+=	-specs=${SPECS_LIBGCC}
-.endif
+_WRAP_EXTRA_ARGS.CC+=	-specs=${SPECS_PKGSRC}
+_WRAP_EXTRA_ARGS.CXX+=	-specs=${SPECS_PKGSRC}
+_WRAP_EXTRA_ARGS.FC+=	-specs=${SPECS_PKGSRC}
+CWRAPPERS_APPEND.cc+=	-specs=${SPECS_PKGSRC}
+CWRAPPERS_APPEND.cxx+=	-specs=${SPECS_PKGSRC}
+CWRAPPERS_APPEND.f77+=	-specs=${SPECS_PKGSRC}
 
 .include "../../mk/dlopen.buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
