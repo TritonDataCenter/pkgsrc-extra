51e7c8f7e54789951d1f95c141eeeaf9502c66ba
Jonathan Perkin <jonathan@perkin.org.uk>
2024-03-13 17:16:40 +0000

    gcc13: Use the DragonFly locale backend on illumos.
    Fixes issue reported on smartos-discuss where running mkvmerge from
    mkvtoolnix aborts if using anything other than a "C" locale.  Some
    support has to be stubbed out due to missing localeconv_l(), but this
    is only in LC_MONETARY and LC_NUMERIC.

---
+++ gcc13/patches/patch-libstdc++-v3_config_locale_dragonfly_numeric__members.cc
@@ -0,0 +1,60 @@
+$NetBSD$
+
+Use the DragonFly locale backend on illumos.
+
+--- libstdc++-v3/config/locale/dragonfly/numeric_members.cc.orig	2024-03-13 11:39:16.206126787 +0000
++++ libstdc++-v3/config/locale/dragonfly/numeric_members.cc
+@@ -44,8 +44,10 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
+       if (!_M_data)
+ 	_M_data = new __numpunct_cache<char>;
+ 
++#if !defined(__illumos__)
+       if (!__cloc)
+ 	{
++#endif
+ 	  // "C" locale
+ 	  _M_data->_M_grouping = "";
+ 	  _M_data->_M_grouping_size = 0;
+@@ -59,6 +61,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
+ 
+ 	  for (size_t __j = 0; __j < __num_base::_S_iend; ++__j)
+ 	    _M_data->_M_atoms_in[__j] = __num_base::_S_atoms_in[__j];
++#if !defined(__illumos__)
+ 	}
+       else
+ 	{
+@@ -114,6 +117,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
+ 	      _M_data->_M_grouping_size = __len;
+ 	    }
+ 	}
++#endif
+ 
+       // NB: There is no way to extact this info from posix locales.
+       // _M_truename = __nl_langinfo_l(YESSTR, __cloc);
+@@ -140,8 +144,10 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
+       if (!_M_data)
+ 	_M_data = new __numpunct_cache<wchar_t>;
+ 
++#if !defined(__illumos__)
+       if (!__cloc)
+ 	{
++#endif
+ 	  // "C" locale
+ 	  _M_data->_M_grouping = "";
+ 	  _M_data->_M_grouping_size = 0;
+@@ -158,6 +164,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
+ 	  for (size_t __j = 0; __j < __num_base::_S_iend; ++__j)
+ 	    _M_data->_M_atoms_in[__j] =
+ 	      static_cast<wchar_t>(__num_base::_S_atoms_in[__j]);
++#if !defined(__illumos__)
+ 	}
+       else
+ 	{
+@@ -212,6 +219,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
+ 	      _M_data->_M_grouping_size = __len;
+ 	    }
+ 	}
++#endif
+ 
+       // NB: There is no way to extact this info from posix locales.
+       // _M_truename = __nl_langinfo_l(YESSTR, __cloc);
