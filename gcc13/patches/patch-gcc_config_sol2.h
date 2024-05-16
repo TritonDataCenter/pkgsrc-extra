500d3d50f26538c4b57810f35986f4a181753a3b
Jonathan Perkin <jonathan@perkin.org.uk>
2024-05-16 11:28:03 +0100

    gcc13: Test __STDC_VERSION__ C++ removal.

--- gcc13/patches/patch-gcc_config_sol2.h
+++ gcc13/patches/patch-gcc_config_sol2.h
@@ -3,9 +3,32 @@ $NetBSD$
 Add ${PREFIX}/lib to the default library search path.  These changes are
 removed by subst if USE_GCC_LOCALBASE_PREFIX == "no".
 
---- gcc/config/sol2.h.orig	2021-09-01 10:28:37.000000000 +0000
+--- gcc/config/sol2.h.orig	2023-08-04 21:07:54.000000000 +0000
 +++ gcc/config/sol2.h
-@@ -308,8 +308,9 @@ along with GCC; see the file COPYING3.
+@@ -95,22 +95,6 @@ along with GCC; see the file COPYING3.
+        library.  */					\
+     if (c_dialect_cxx ())				\
+       {							\
+-	switch (cxx_dialect)				\
+-	  {						\
+-	  case cxx98:					\
+-	  case cxx11:					\
+-	  case cxx14:					\
+-	    /* C++11 and C++14 are based on C99.	\
+-	       libstdc++ makes use of C99 features	\
+-	       even for C++98.  */			\
+-	    builtin_define ("__STDC_VERSION__=199901L");\
+-	    break;					\
+-							\
+-	  default:					\
+-	    /* C++17 is based on C11.  */		\
+-	    builtin_define ("__STDC_VERSION__=201112L");\
+-	    break;					\
+-	  }						\
+ 	builtin_define ("_XOPEN_SOURCE=600");		\
+ 	builtin_define ("_LARGEFILE_SOURCE=1");		\
+ 	builtin_define ("_LARGEFILE64_SOURCE=1");	\
+@@ -308,8 +292,9 @@ along with GCC; see the file COPYING3.
  #define LINK_ARCH32_SPEC_BASE \
    "%{YP,*} \
     %{R*} \
@@ -17,7 +40,7 @@ removed by subst if USE_GCC_LOCALBASE_PREFIX == "no".
  
  #undef LINK_ARCH32_SPEC
  #define LINK_ARCH32_SPEC LINK_ARCH32_SPEC_BASE
-@@ -320,8 +321,9 @@ along with GCC; see the file COPYING3.
+@@ -320,8 +305,9 @@ along with GCC; see the file COPYING3.
  #define LINK_ARCH64_SPEC_BASE \
    "%{YP,*} \
     %{R*} \
