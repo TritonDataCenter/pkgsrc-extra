1b0e086aacea45ba2ba4b0f8ab0e645ccd68f479
Jonathan Perkin <jonathan@perkin.org.uk>
2024-10-29 13:16:50 +0000

    wireguard-go: Update to 0.0.20230223.

--- wireguard-go/go-modules.mk
+++ wireguard-go/go-modules.mk
@@ -2,15 +2,15 @@
 
 GO_MODULE_FILES+=	github.com/google/btree/@v/v1.0.1.mod
 GO_MODULE_FILES+=	github.com/google/btree/@v/v1.0.1.zip
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20220315160706-3147a52a75dd.mod
-GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20220315160706-3147a52a75dd.zip
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20220225172249-27dd8689420f.mod
-GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20220225172249-27dd8689420f.zip
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20221010170243-090e33056c14.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20221010170243-090e33056c14.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.6.0.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.6.0.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.7.0.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.7.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.5.1-0.20230222185716-a3b23cc77e89.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.5.1-0.20230222185716-a3b23cc77e89.zip
 GO_MODULE_FILES+=	golang.org/x/time/@v/v0.0.0-20191024005414-555d28b269f0.mod
 GO_MODULE_FILES+=	golang.org/x/time/@v/v0.0.0-20191024005414-555d28b269f0.zip
-GO_MODULE_FILES+=	golang.zx2c4.com/wintun/@v/v0.0.0-20211104114900-415007cec224.mod
-GO_MODULE_FILES+=	golang.zx2c4.com/wintun/@v/v0.0.0-20211104114900-415007cec224.zip
-GO_MODULE_FILES+=	gvisor.dev/gvisor/@v/v0.0.0-20220817001344-846276b3dbc5.mod
-GO_MODULE_FILES+=	gvisor.dev/gvisor/@v/v0.0.0-20220817001344-846276b3dbc5.zip
+GO_MODULE_FILES+=	golang.zx2c4.com/wintun/@v/v0.0.0-20230126152724-0fa3db229ce2.mod
+GO_MODULE_FILES+=	golang.zx2c4.com/wintun/@v/v0.0.0-20230126152724-0fa3db229ce2.zip
+GO_MODULE_FILES+=	gvisor.dev/gvisor/@v/v0.0.0-20221203005347-703fd9b7fbc0.mod
+GO_MODULE_FILES+=	gvisor.dev/gvisor/@v/v0.0.0-20221203005347-703fd9b7fbc0.zip
