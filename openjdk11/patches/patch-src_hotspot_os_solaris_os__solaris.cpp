fb4edc5495c7acdf5583c3b67a676cb54cd0092a
Jonathan Perkin <jonathan@perkin.org.uk>
2026-07-24 09:51:12 +0100

    openjdk*: Remove conflicting memcntl() prototype.
    Previously we tried to match but we're going with upstream gate for new
    hosts and this can't support both.

--- openjdk11/patches/patch-src_hotspot_os_solaris_os__solaris.cpp
+++ openjdk11/patches/patch-src_hotspot_os_solaris_os__solaris.cpp
@@ -1,15 +1,14 @@
 $NetBSD$
 
-Match original caddr_t prototype as we still ship the original headers.
+Remove conflicting memcntl prototype.
 
 --- src/hotspot/os/solaris/os_solaris.cpp.orig	2023-12-18 11:45:57.461845536 +0000
 +++ src/hotspot/os/solaris/os_solaris.cpp
-@@ -169,7 +169,7 @@ extern "C" {
+@@ -169,7 +169,6 @@ extern "C" {
  
    static int lwp_cond_init(cond_t *cv, int scope, void *arg){ memset(cv, 0, sizeof(cond_t)); return 0; }
    static int lwp_cond_destroy(cond_t *cv)                   { return 0; }
 -  int memcntl(void *, size_t, int, void *, int, int);
-+  int memcntl(caddr_t, size_t, int, caddr_t, int, int);
    int meminfo(const uint64_t *, int, const uint_t *, int, uint64_t *, uint_t *);
  }
  
